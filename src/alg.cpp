0e1fe8231febd09b99832d53d1482e4f33dab7e6
Galkina-Darya <99248872+Galkina-Darya@users.noreply.github.com>
2022-02-19 22:25:42 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -17,8 +17,8 @@ bool checkPrime(uint64_t value) {
 
 uint64_t nPrime(uint64_t n) {
 // вставьте код функции
-   uint64_t count = 0;
-    uint64_t i = 0;
+      uint64_t count = 0;
+      uint64_t i = 0;
     for (uint64_t j = 2 ; j <= LONG_MAX ; j++) {
         if (checkPrime(j)) {
             i += 1;
@@ -33,8 +33,8 @@ return count;
 
 uint64_t nextPrime(uint64_t value) {
 // вставьте код функции
-   uint64_t count = 0;
-    uint64_t i = 0;
+      uint64_t count = 0;
+      uint64_t i = 0;
      for (uint64_t k = value + 1 ; k <= LONG_MAX ; k++) {
         if (checkPrime(k)) {
             count = k;
@@ -49,9 +49,7 @@ uint64_t sumPrime(uint64_t hbound) {
       uint64_t sum = 0;
     for (uint64_t m = 2 ; m < hbound ; m++) {
         if (checkPrime(m)) {
-             {
                 sum += m;
-            }
         }
     }
     return sum;
